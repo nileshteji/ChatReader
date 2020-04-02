de48301c8d2eebe735423b2638c03badef141bfb
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-04-02 22:20:57 +0530

    Version 1.1.

--- Blushed/app/src/main/java/com/osos/blushed/MainActivity.java
+++ Blushed/app/src/main/java/com/osos/blushed/MainActivity.java
@@ -27,7 +27,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewOutlineProvider;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Message> oldList;
     ArrayList<Message> newList;
     RecyclerView recyclerView;
+    ListView listView;
     int index=0;
 
     @Override
@@ -45,12 +48,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         constraintLayout = findViewById(R.id.con);
+        listView=findViewById(R.id.list);
         recyclerView=findViewById(R.id.rec);
         Adapter adapter=new Adapter(MainActivity.this,new Data().getList());
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager(MainActivity.this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
+listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(MainActivity.this, index, Toast.LENGTH_SHORT).show();
+        index++;
+    }
+});
+//
+//        recyclerView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, index, Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
 
 
